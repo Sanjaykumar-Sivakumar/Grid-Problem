8f873dbd040caa51ad96967bc642a68cb58879d8
SANJAYKUMAR S <sanjays23112004@gmail.com>
2025-11-21 15:00:23 +0530

    Update GridGenerator.java

--- GridGenerator.java
+++ GridGenerator.java
@@ -1,18 +1,11 @@
-
 import java.util.*;
-
 public class GridGenerator {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int n = 0;
-
-        
         while (true) {
             System.out.print("Enter N: ");
             String s = sc.nextLine();
-
             try {
                 n = Integer.parseInt(s);
                 if (n > 0) {
@@ -24,18 +17,14 @@ public class GridGenerator {
                 System.out.println("Invalid input");
             }
         }
-
         int[][] arr = new int[n][n];
         Random r = new Random();
-
-        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 int num = r.nextInt(10) * 2 + 2; // 2 to 20 even
                 arr[i][j] = num;
             }
         }
-
         System.out.println("\nGrid:");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -44,9 +33,7 @@ public class GridGenerator {
             System.out.println();
         }
 
-        int x = 0;
-
-        
+        int x = 0;        
         while (true) {
             System.out.print("\nSelect a number (2 to 20 even): ");
             String s2 = sc.nextLine();
@@ -61,9 +48,7 @@ public class GridGenerator {
             } catch (Exception e) {
                 System.out.println("Invalid input");
             }
-        }
-
-       
+        }     
         int cnt = 0;
         System.out.println("\nHighlighted Grid:");
         for (int i = 0; i < n; i++) {
@@ -77,8 +62,8 @@ public class GridGenerator {
             }
             System.out.println();
         }
-
         System.out.println("\nThe number " + x + " occurred " + cnt + " times.");
 
     }
 }
+
